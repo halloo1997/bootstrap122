7bb3cb158a1fcbd020ec7af70ace86d7271c6a87
chariesa <chariesa@hotmail.com>
2018-10-13 15:10:20 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">
+                    <img src="1280px-NOS_logo.svg.png" alt="Logo" class="img-fluid" width="100">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <ol class="breadcrumb">
+                    <li><a class="nav-link" href="#">Nieuws </a></li>
+                    <li><a class="nav-link" href="#">Sport</a></li>
+                    <li><a class="nav-link" href="#">Uitzendingen</a></li>
+                </ol>
+
+            </nav>
+            <h1>Moeder van Hugo Borst overleden</h1>
+            <time datetime="2018-08-18T08:17:31+0200">Za 18 augustus, 08:17 </time><a class="link-grey">Binnenland</a>
+            <img src="xxl.jpg"  class="img-responsive center-block" >
+            <blockquote style="height: 62px; width: 801px;">Hugo Borst in 2017 tijdens de presentatie van zijn nieuwe boek Ach, moedertje ANP<footer>Hugo Borst</footer></blockquote>
+        <p>De moeder van schrijver, columnist en NOS-presentator Hugo Borst is in een verpleegtehuis in Rotterdam overleden. Joke Borst was 89 jaar. Hugo schreef elke week over haar onder de kop "Mijn ma" in het AD-magazine. Zij leed aan alzheimer.</p>
+        <p>Borsts woede over de behandeling die zijn moeder onderging, inspireerde hem en onderzoeker Carin Gaemers in 2016 tot het schrijven van een open brief aan de toenmalige staatssecretaris van Volksgezondheid, Van Rijn. Daarin veroordeelden zij het tekort aan personeel in de verpleegzorg en de vele administratieve taken die de verzorgenden moesten verrichten. De patiënten kregen daardoor niet de zorg en veiligheid die ze nodig hadden.</p>
+        <p>Na aandrang van de Tweede Kamer maakte de Inspectie voor de Gezondheidszorg bekend dat 150 verpleeghuizen niet de zorg leverden waarop gerekend mocht worden.</p>
+        <p><strong>Manifest</strong></p>
+        <p>Borst en Gaemers schreven vervolgens het manifest Scherp op Ouderenzorg. Daarin werd de politiek opgeroepen om zo snel mogelijk goede zorg voor alle kwetsbare ouderen in verpleegtehuizen te waarborgen.</p>
+        <p>Het manifest kreeg meer dan 100.000 steunbetuigingen en leidde ertoe dat het kabinet 2,1 miljard euro extra voor verpleeghuiszorg beschikbaar stelde.</p>
+        </div>
+    </div>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
